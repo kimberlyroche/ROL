035f1c64cba3a1ebdffbe71b18ab388bd218383c
Kim Roche <kimberly.e.roche@gmail.com>
2020-05-19 18:21:44 -0400

    Added beginnings of covariate EDA

--- exploratory/dendrogram.R
+++ exploratory/dendrogram.R
@@ -1,13 +1,17 @@
 library(ROL)
-library(driver)
-library(dplyr)
+# library(driver)
+library(tidyverse)
 library(phyloseq)
 library(dendextend)
 
 tax_level <- "ASV"
 
-heatmap <- plot_interaction_heatmap(tax_level=tax_level, logratio="clr", Sigmas=NULL,
+Sigmas <- load_MAP_estimates(tax_level = tax_level, logratio = "clr")
+interactions <- plot_interaction_heatmap(tax_level=tax_level, logratio="clr", Sigmas=NULL,
                                          taxon_idx=NULL, show_plot=FALSE, return_matrix=TRUE)
+# THE ABOVE INTERACTIONS HAVE THE HOSTS REORDERED BY HCLUST!!!
+# LABELING VIA names(Sigma) WILL BE WRONG
+interactions <- get_pairwise_correlations(tax_level = tax_level, logratio = "clr", Sigmas=NULL)$interactions
 
 model_list <- get_fitted_model_list(tax_level=tax_level, MAP=TRUE)
 
@@ -33,7 +37,7 @@ for(host in hosts) {
 labels <- as.factor(labels)
 
 # cluster and visualize hosts
-dd <- dist(heatmap, method = "euclidean")
+dd <- dist(interactions)
 dmat <- as.matrix(dd)
 
 d_labels <- sapply(hosts, function(x) labels[[x]] )
@@ -42,10 +46,31 @@ rownames(dmat) <- d_labels
 colnames(dmat) <- d_labels
 hc <- hclust(as.dist(dmat))
 dend <- as.dendrogram(hc)
-labels_colors(dend) <- as.numeric(as.factor(hc$labels))
+#labels_colors(dend) <- as.numeric(as.factor(hc$labels))
 dend <- set(dend, "labels_cex", 1)
 
-png("output/plots/host_interaction_dendrogram.png", height=1000, width=1000)
+#png("output/plots/host_interaction_dendrogram.png", height=1000, width=1000)
+png("test_2.png", height=1000, width=1000)
 plot(dend)
+dev.off()
+
+# JOHANNES' VERSION WORKS CORRECTLY
+
+# Johannes' assignments; these look 100% the same
+primary_group <- setNames(metadata %>% 
+  group_by(sname, grp) %>%
+  tally() %>%
+  filter(n==max(n)) %>%
+  select(grp) %>% pull(), 
+  metadata %>% 
+  group_by(sname, grp) %>%
+  tally() %>%
+  filter(n==max(n)) %>%
+  select(sname) %>% pull())
+
+rownames(interactions) <- paste(hosts, unname(primary_group), sep="_")
+
+png("test_3.png"), height=1000, width=1000)
+plot(ape::as.phylo(hclust(dist(interactions))), cex=1)
 dev.off()
 
