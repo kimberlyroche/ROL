e0d5ce4156d852f8b23e5bebeed2e9ad7facf15b
Kimberly Roche <ker48@c1-01-4.genome.duke.edu>
2020-07-14 16:43:51 -0400

    Local HARDAC changes

--- scripts/cv.R
+++ scripts/cv.R
@@ -2,7 +2,7 @@ library(ROL)
 library(phyloseq)
 library(filelock)
 
-#args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly=TRUE)
 
 if(length(args) < 1) {
   stop("Missing argument for host!\n")
@@ -17,6 +17,14 @@ record_error <- function(results, days_to_baseline, incl_covariates, Gamma_scale
                incl_covariates,"\t",Gamma_scale,"\t",Xi_scale), file = out_file, append = TRUE)
   write(paste0(round(results$log_rmse,5),"\tlog rmse\t",host,"\t",days_to_baseline,"\t",
                incl_covariates,"\t",Gamma_scale,"\t",Xi_scale), file = out_file, append = TRUE)
+  unlock(lck)
+}
+
+record_failed_fit <- function(out_str) {
+  out_file <- file.path("output","cv_failed_fits.txt")
+  lock_file <- paste0(out_file, ".lck")
+  lck <- lock(lock_file, timeout = Inf)
+  write(out_str, file = out_file, append = TRUE)
   unlock(lck)
 }
 
@@ -43,8 +51,18 @@ for(days_to_baseline in c(90, 30)) {
         sample_covariance <- get_Gamma(kernel_scale = Gamma_scale, proportions = kernel_proportions,
                                min_correlation = 0.1, days_to_baseline = days_to_baseline)
         taxa_covariance <- get_Xi(phyloseq::ntaxa(data), total_variance = Xi_scale)
-        err <- perform_cv(data, host, taxa_covariance, sample_covariance, tax_level, holdout_proportion = 0.2)
-        record_error(err, days_to_baseline, incl_covariates, Gamma_scale, Xi_scale)
+        # this can bomb if model fit fails to converge during optimization; for now, just pass over this case
+        # we need to think about whether this is systematically affecting error in these cases
+        err = tryCatch({
+          perform_cv(data, host, taxa_covariance, sample_covariance, tax_level, holdout_proportion = 0.2)
+        }, error = function(e) {
+          # write to file
+          out_str <- paste0(host,"\t",days_to_baseline,"\t",incl_covariates,"\t",Gamma_scale,"\t",Xi_scale)
+          record_failed_fit(out_str)
+        }, finally = {})
+        if(!is.null(err)) {
+          record_error(err, days_to_baseline, incl_covariates, Gamma_scale, Xi_scale)
+        }
       }
     }
   }
--- scripts/plot_cv.R
+++ scripts/plot_cv.R
@@ -1,26 +1,45 @@
 library(ggplot2)
 
+label_run <- function(run_combo) {
+  paste0("days",run_combo$days_decay,"_",
+         "cov",run_combo$covariates,"_",
+         "Gamma",run_combo$Gamma_scale,"_",
+         "Xi",run_combo$Xi_scale)
+}
+
 cv_results <- read.table("output/cv_results.txt", header = FALSE, stringsAsFactors = TRUE, sep = "\t")
-colnames(cv_results) <- c("value", "error_type", "host", "run_label")
+colnames(cv_results) <- c("value", "error_type", "host", "days_decay", "covariates", "Gamma_scale", "Xi_scale")
+head(cv_results)
 
 # stick with log RMSE for now
 error_type <- "log rmse"
 cv_results <- cv_results[cv_results$error_type == error_type,]
 
-# visualize error by model type ("run_label")
-p <- ggplot(cv_results, aes(x = run_label, y = value)) +
+summ_df <- data.frame(error = c(), model = c())
+run_combos <- unique(cv_results[,c("days_decay","covariates","Gamma_scale","Xi_scale")])
+for(cidx in 1:nrow(run_combos)) {
+  rc <- run_combos[cidx,]
+  result_subset <- cv_results[(cv_results$days_decay == rc$days_decay &
+                               cv_results$covariates == rc$covariates &
+                               cv_results$Gamma_scale == rc$Gamma_scale &
+                               cv_results$Xi_scale == rc$Xi_scale),]
+  summ_df <- rbind(summ_df, data.frame(error = result_subset[result_subset$error_type == "log rmse",]$value,
+                                       model = label_run(result_subset[1,])))
+}
+
+p <- ggplot(summ_df, aes(x = model, y = error)) +
   geom_boxplot() +
   xlab("Model type") +
-  ylab("RMSE of log counts")
+  ylab("RMSE of log counts") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 ggsave("runlabel_boxplot.png", p, units = "in", dpi = 150, height = 6, width = 8)
 
 # visualize error by host for each model type
-for(rl in unique(cv_results$run_label)) {
-    p <- ggplot(cv_results[cv_results$run_label == rl,], aes(x = host, y = value)) +
-                geom_boxplot() +
-                theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-                xlab("Host") +
-                ylab(paste0("RMSE of log counts (model \"",rl,"\")"))
-    ggsave(paste0("host_boxplot_",rl,".png"), p, units = "in", dpi = 150, height = 8, width = 12)
-}
+# p <- ggplot(cv_results[cv_results$run_label == rl,], aes(x = host, y = value)) +
+#             geom_boxplot() +
+#             theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+#             xlab("Host") +
+#             ylab(paste0("RMSE of log counts (model \"",rl,"\")"))
+# ggsave(paste0("host_boxplot_",rl,".png"), p, units = "in", dpi = 150, height = 8, width = 12)
+
 
